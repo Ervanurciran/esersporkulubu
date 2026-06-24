24817376c8c1c0ec7a4b8258d7f3c9c737042e27
Berkay Ayçiçek <berkayaycicek2@gmail.com>
2026-06-24 17:53:29 +0300

    Use project-specific admin seed env keys

--- config/admin.php
+++ config/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'email' => env('ADMIN_EMAIL', 'admin@eserspor.com'),
-    'password' => env('ADMIN_PASSWORD', 'admin123456'),
+    'email' => env('ESERSPOR_ADMIN_EMAIL', 'admin@eserspor.com'),
+    'password' => env('ESERSPOR_ADMIN_PASSWORD', 'admin123456'),
 ];
